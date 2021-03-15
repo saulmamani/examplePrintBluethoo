7cf0acc07bf0be82b2ff486b0043baf3276b0b2a
saulmamani <luas0_1@yahoo,es>
2021-03-15 11:07:59 -0400

    zebra librari

--- app/src/main/java/com/example/testzebra/MainActivity.kt
+++ app/src/main/java/com/example/testzebra/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.testzebra
 
 import android.bluetooth.BluetoothAdapter
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
-import android.media.Image
 import android.os.Bundle
+import android.os.Looper
 import android.text.Spanned
 import android.util.Log
 import android.view.View
@@ -15,12 +14,21 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
+import com.zebra.sdk.comm.BluetoothConnection
+import com.zebra.sdk.comm.Connection
+import com.zebra.sdk.comm.ConnectionException
+import com.zebra.sdk.comm.TcpConnection
+import com.zebra.sdk.graphics.ZebraImageFactory
+import com.zebra.sdk.graphics.ZebraImageI
+import com.zebra.sdk.printer.ZebraPrinter
+import com.zebra.sdk.printer.ZebraPrinterFactory
+import com.zebra.sdk.printer.ZebraPrinterLanguageUnknownException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
-import java.io.InputStream
+import java.io.IOException
 import java.io.OutputStream
 import java.util.*
 
@@ -35,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val sourceDetail: String = "<div style='text-align:center'>" +
-                "<h1>FACTURA #146</h1> <br/>" +
+                "<h1>FACTURA #195</h1> <br/>" +
                 "<h4>Catastro: C023D3401-S</h4>" +
                 "</div>" +
                 "<h5>Cliente: Saul Mamani</h4>" +
@@ -47,15 +55,96 @@ class MainActivity : AppCompatActivity() {
         val detalle: Spanned = HtmlCompat.fromHtml(sourceDetail, HtmlCompat.FROM_HTML_MODE_LEGACY);
         findViewById<TextView>(R.id.txtMessage).text = detalle;
 
-        //imprimiendo imagen
+        findViewById<Button>(R.id.btnImprimirEjemplo).setOnClickListener{
+            //imprimiendo imagen
+            val example = MainActivity()
+
+            val theBtMacAddress = "CC:78:AB:A0:49:BA"
+            example.sendCpclOverBluetooth(theBtMacAddress)
+        }
+
         findViewById<Button>(R.id.btnImprimirImagen).setOnClickListener{
-            val sourceLogo = findViewById<ImageView>(R.id.imgLogo);
-            val logo = this.imageToBitmap(sourceLogo);
-            outputStream?.run {
-                write(logo)
-            }
+            //imprimiendo imagen
+            val example = MainActivity()
+
+            val theBtMacAddress = "CC:78:AB:A0:49:BA"
+            example.printImage(theBtMacAddress)
+        }
+
+    }
+
+    private fun printImage(mac: String){
+        val connection: Connection = BluetoothConnection(mac)
+        try {
+            connection.open()
+            val printer: ZebraPrinter = ZebraPrinterFactory.getInstance(connection)
+            val x = 0
+            val y = 0
+//            val imagen = applicationContext.resources.getResourceName(R.drawable.logo_200w)
+//            val bitmap: Bitmap = (imagen.drawable as BitmapDrawable).bitmap
+            printer.printImage("/home/saul/Escritorio/uno.jpg", x, y)
+        } catch (e: ConnectionException) {
+            e.printStackTrace()
+        } catch (e: ZebraPrinterLanguageUnknownException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            connection.close()
         }
     }
+
+    private fun sendCpclOverBluetooth(mac: String) {
+        Thread(Runnable {
+            try {
+                // Instantiate connection for given Bluetooth&reg; MAC Address.
+                val thePrinterConn: Connection =
+                    BluetoothConnection(mac)
+
+                // Initialize
+                Looper.prepare()
+                // Open the connection - physical connection is established here.
+                thePrinterConn.open()
+
+                // This example prints "This is a CPCL test." near the top of the label.
+                //                PCX 0 1 !<B.PCX
+//                T 5 0 125 179 I love saltenias
+//                T 5 0 125 217 I love tucumanas
+
+                //TEXT 4 0 30 40 MOLA MOLA....
+                //! 0 200 200 210 1
+                val cpclData = """
+                    ! 0 200 200 600 1
+                    T 4 0 125 160        Empresa: SeLA Oruro
+                    T 5 0 125 179        Time: 12:12
+                    T 5 0 125 217        User: @cliente
+                    T 5 0 125 236        Details
+                    T 5 0 125 255        Card Price: 2213
+                    T 5 0 125 274        Balance: 33
+                    T 5 0 25 293             Gracias
+                    T 5 0 40 312                   www.aa.aa.aa
+                    FORM
+                    PRINT
+                    
+                    """.trimIndent().replace("@cliente", "Zoyla del Rincon")
+
+                // Send the data to printer as a byte array.
+                thePrinterConn.write(cpclData.toByteArray())
+
+                // Make sure the data got to the printer before closing the connection
+                Thread.sleep(500)
+
+                // Close the connection to release resources.
+                thePrinterConn.close()
+                Looper.myLooper()!!.quit()
+            } catch (e: java.lang.Exception) {
+
+                // Handle communications error here.
+                e.printStackTrace()
+            }
+        }).start()
+    }
+
 
     public fun conectar(v: View) {
         GlobalScope.launch(Dispatchers.Main) {
@@ -87,12 +176,35 @@ class MainActivity : AppCompatActivity() {
         outputStream?.write("Ya pues!\n".toByteArray())
     }
 
-    private fun imageToBitmap(image: ImageView): ByteArray {
-        val bitmap = (image.drawable as BitmapDrawable).bitmap
+    private fun imageToBitmap(image: ImageView): ByteArray? {
+        Toast.makeText(this@MainActivity, "Imprimiendo...", Toast.LENGTH_SHORT)
+            .show()
+        // val bitmap:Bitmap = image.drawable.toBitmap()
+
+//        val bitmap = Bitmap.createBitmap(image.width, image.height, Bitmap.Config.RGB_565)
+//Uri.parse("drawable/andro.png")
+
+        val bitmap = (image as BitmapDrawable).bitmap
         val stream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
 
         return stream.toByteArray()
+        //  return bmpToByteArray(bitmap, false)
+    }
+
+    fun bmpToByteArray(bmp: Bitmap, needRecycle: Boolean): ByteArray? {
+        val output = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.JPEG, 60, output)
+        if (needRecycle) {
+            bmp.recycle()
+        }
+        val result = output.toByteArray()
+        try {
+            output.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+        return result
     }
 
     private suspend fun connect(): OutputStream? {
